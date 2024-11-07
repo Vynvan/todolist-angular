d06b48330dcc1e23382a1c5d20f7e0b9dde14b0e
vynvan_server <giant_innovations@live.de>
2024-11-07 15:35:20 +0100

    Added nav and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavigationComponent } from '../components/navigation/navigation.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+   selector: 'app-root',
+   standalone: true,
+   imports: [
+      CommonModule,
+      FooterComponent,
+      NavigationComponent,
+      RouterOutlet
+   ],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todolist-angular';
+   title = 'todolist-angular';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './pages/todolist/todolist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: AppComponent, children: [
+      { path: 'Todolist', component: TodolistComponent },
+   ]},
+];
